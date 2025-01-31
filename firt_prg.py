86d16a2c2754eabf036a6e1e0318a2188c9ec52c
Rabi Sah <rabindrasah1997.1@gmail.com>
2025-01-31 18:53:58 +0545

    first commit

---
+++ firt_prg.py
@@ -0,0 +1,50 @@
+# print("Hello from the file python")
+# # Area of a rectangle
+# length = 10
+# width = 5
+# area = length * width
+# print("The area of the rectangle is", area)
+# # Area of a circle
+# radius = 5
+# pi = 3.14
+# area = pi * radius ** 2
+# print("The area of the circle is", area)
+# # Area of a triangle
+# base =89
+# height=90
+# area =1/2*(base*height)
+# print("The area of the triangle is", area) 
+# #Area of triangle using input
+# base=int(input("Enter the base of the triangle: ")) 
+# height=int(input("Enter the height of the triangle: ")) 
+# area =1/2*(base*height) 
+# print("The area of the triangle is", area)
+#  # check odd and even number
+# num =int(input("Enter a number:"))
+# if num%2==0:
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+
+# check the greatest amon three numbers
+# num1= int(input("Enter the first number: "))
+# num2=int(input("Enter the second number: "))
+# num3=int(input("Enter the third number: "))
+# if num1>num2 and num1>num3:
+#     print("The greatest number is", num1)
+# elif num2>num1 and num2>num3:
+#     print("The greatest number is ", num2)
+# else:
+#     print("The greatest number is ", num3)  
+
+#for loop
+# exp=[100,200,300,400,500]
+# total=0  
+# for i in exp:
+#     total=total+i
+#     print("The total of the expenses is", total)    
+#print the sum of the first 10 numbers
+total=0
+for i in range(1,11):
+    total=total+i
+    print("The total of the first 10 numbers is", total)
